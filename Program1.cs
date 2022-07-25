7b5cd609e248b7965e08fcc8405d48576f80d987
Никита <109956617+nikitgithub@users.noreply.github.com>
2022-07-25 14:45:11 +0300

    Add files via upload
    модуль 1

---
+++ Program1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika
+{
+    internal class Program
+    {   
+        static void data()
+        {
+            Console.WriteLine("Hello World !!!");
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write("!!!");
+        }
+        static void Main(string[] args)
+        {
+          data();
+            Console.ReadLine();
+        }
+    }
+}
