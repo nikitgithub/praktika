15782193201086589e767363645e57de880dd897
Никита <109956617+nikitgithub@users.noreply.github.com>
2022-07-25 14:39:21 +0300

    Add files via upload
    модуль №2

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika
+{
+    internal class Program
+    {   
+        static void data()
+        {
+            string FullName = "Галимзянов Никита Сергеевич";
+            int age = 18;
+            string email = "n_kzn@mail.ru";
+            int ProgrammingScores = 96;
+            int MathScores = 95;
+            int PhysicsScores = 100;
+            Console.WriteLine($" Ф.И.О:{FullName},\n возраст:{age},\n email:{email},\n балл по информатике:{ProgrammingScores},\n балл по математике:{MathScores},\n балл по физике:{PhysicsScores} ");
+        }
+        static void Ball(int Value_1,int Value_2,int Value_3)
+        {
+            int score;
+            score = (Value_1 + Value_2 + Value_3) / 3;
+            Console.WriteLine($" средний балл:{score}");
+
+        }
+        static void Main(string[] args)
+        {
+            data();
+            Ball(96,95,100);
+            Console.ReadLine();
+            
+        }
+    }
+}
